99e3dab4c88b20f7e975e10e6bbd095bbaf75d4d
Avinav-Dasgupta <avinavdas17@gmail.com>
2025-12-15 21:28:50 +0530

    Made Basic Todo List

---
+++ Todo-List/script.js
@@ -0,0 +1,31 @@
+const input = document.getElementById("inputBox");
+const list = document.getElementById("list");
+const submitButton = document.getElementById("submit");
+
+function addTask() {
+    const text = input.value;
+    if (text == "") {
+        alert("Task is empty");
+        return;
+    }
+    else {
+        const listChild = document.createElement("li");
+        const deleteButton = document.createElement("button");
+        listChild.textContent = text + " ";
+        deleteButton.textContent = "delete";
+        listChild.appendChild(deleteButton);
+        list.appendChild(listChild);
+        deleteButton.addEventListener("click", () => {
+            listChild.remove();
+        })
+    }
+    input.value = "";
+
+}
+
+submitButton.addEventListener("click", addTask);
+input.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        addTask();
+    }
+})
