3cd58fc35115d856d5f7cd9b243c40eaa73d87b4
Avinav-Dasgupta <avinavdas17@gmail.com>
2025-12-22 19:19:02 +0530

    Made Basic Star Rating

---
+++ Star-Rating/script.js
@@ -0,0 +1,52 @@
+const star = `<svg xmlns="http://www.w3.org/2000/svg" class="star-icon"  viewBox="0 0 24 24"
+            stroke="currentColor" stroke-width="2">
+            <path stroke-linecap="round" stroke-linejoin="round"
+                d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z" />
+        </svg>` ;
+
+const container = document.getElementById("box");
+const stars = [];
+let filledStars = 0;
+
+function removeFill() {
+    for (let i = 0; i < 5; i++) {
+        stars[i].classList.add("empty");
+        stars[i].classList.remove("filled");
+    }
+}
+
+function addFill(star) {
+    for (let i = 0; i <star; i++) {
+        stars[i].classList.add("filled");
+        stars[i].classList.remove("empty");
+    }
+}
+
+function displayStars() {
+    for (let i = 0; i < 5; i++) {
+        const starSpace = document.createElement("div");
+        starSpace.innerHTML = star;
+        starSpace.classList.add("empty");
+        
+        starSpace.addEventListener("click", () => {
+            removeFill();
+            addFill(i+1);
+            filledStars = i+1;
+        });
+
+        starSpace.addEventListener("mouseover", () =>{
+            removeFill();
+            addFill(i+1);
+        })
+        container.appendChild(starSpace);
+        stars.push(starSpace);
+    }
+}
+
+container.addEventListener("mouseleave", () =>{
+    removeFill();
+    addFill(filledStars);
+});
+
+
+displayStars();
